71cd05c1c15260d0bf1d33698c3f6c9bcc648a11
Ariff Rahimin <ariffrahiminmohamed@airasia.com>
2022-11-02 16:14:42 +0800

    Search input creation

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,12 @@ function App() {
         
         <h1 className= 'text-5xl text-center pt-12'>FlagPicker</h1>
       </div>
+      {/* Search Input */}
+      <div className='text-center mt-10'>
+        <label className='relative block'>
+          <input className="placeholder:italic placeholder:text-slate-400 bg-white w-1/3  border border-slate-400 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Country's name..." type="text" name="search"/>
+        </label>
+      </div>
      
     </div>
   );
