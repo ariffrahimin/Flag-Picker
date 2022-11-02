76ebba2f8b91dd616e873c37db723144bc2783ac
Ariff Rahimin <ariffrahiminmohamed@airasia.com>
2022-11-02 18:14:36 +0800

    Search button creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
-function App() {
+const App = () => {
+  
+  const [country,setCountry] = useState("")
+
+  const handleSubmit = () => {
+
+    console.log("handleSubmit success")
+    console.log(country)
+  }
   return (
     <div className="App">
       {/* Title */}
@@ -11,8 +19,9 @@ function App() {
       </div>
       {/* Search Input */}
       <div className='text-center mt-10'>
-        <form className='relative block' >
-          <input className="placeholder:italic placeholder:text-slate-400 bg-white w-1/3  border border-slate-400 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Country's name..." type="text" name="search"/>
+        <form className='relative block' onSubmit={handleSubmit} >
+          <input className="placeholder:italic placeholder:text-slate-400 bg-white w-1/3  border border-slate-400 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Country's name..." value={country} onChange={(e) => setCountry(e.target.value)} type="text" name="search"/>
+          <input type='submit' value = "Search"  className=" text-black font-bold py-2 px-4 border border-black rounded ml-4" />
         </form>
       </div>
      
