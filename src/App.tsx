96556401c960e76de557e102444322b53d895f0b
Ariff Rahimin <ariffrahiminmohamed@airasia.com>
2022-11-02 15:33:56 +0800

    Create Title

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      {/* Title */}
+      <div className='mt-20'>
+        
+        <h1 className= 'text-5xl text-center pt-12'>FlagPicker</h1>
+      </div>
      
     </div>
   );
